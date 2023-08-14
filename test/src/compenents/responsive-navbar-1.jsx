90956418198681e4d0be64fb5c6360f6202cb723
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-08-14 10:53:28 +0200

    navbar update

--- test/src/compenents/responsive-navbar-1.jsx
+++ test/src/compenents/responsive-navbar-1.jsx
@@ -3,28 +3,23 @@ import React from "react";
 
 const Navbar1 = () => {
     return (
-        <div>
-            <div className="container">
-  <div className="row">
-    <div className="menu-btn">
-      <i className="fa-solid fa-bars"></i>
-    </div>
-    <nav className="navbar">
-      <div className="logo">
-
-        <a href="">Your Logo</a>
-      </div>
-      <ul className="main-menu">
-        <li className="list-items"><a href="">About</a></li>
-        <li className="list-items"><a href="">Services</a></li>
-        <li className="list-items"><a href="">Contact</a></li>
-        <li className="list-items"><a href="">Blog</a></li>
-      </ul>
-    </nav>
-  </div>
-
-</div>
-        </div>
+      <>
+      <header>
+  <h1 className="logo"><a href="#">Navbar</a></h1>
+  <input type="checkbox" id="nav-toggle" className="nav-toggle" />
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  <label htmlFor="nav-toggle" title="Show navbar" className="nav-toggle-label">
+    <span></span>
+  </label>
+</header>
+      </>
       )
     }
     export default Navbar1
